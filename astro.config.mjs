a9370fd895d6f9a5e90b1c2acc638dc4f1a86364
Stephan Griesel <sgriesel@gmail.com>
2024-10-23 06:20:50 +0200

    icon config

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,103 +2,89 @@ import { defineConfig } from "astro/config";
 import NetlifyCMS from "astro-netlify-cms";
 import tailwind from "@astrojs/tailwind";
 
+import icon from "astro-icon";
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [
-    tailwind({
-      config: {
-        path: "./custom-config.cjs",
+  integrations: [tailwind({
+    config: {
+      path: "./custom-config.cjs"
+    }
+  }), NetlifyCMS({
+    config: {
+      // Use Netlify’s “Git Gateway” authentication and target our default branch
+      backend: {
+        name: "git-gateway",
+        branch: "latest"
       },
-    }),
-    NetlifyCMS({
-      config: {
-        // Use Netlify’s “Git Gateway” authentication and target our default branch
-        backend: {
-          name: "git-gateway",
-          branch: "latest",
-        },
-        // Logo for backend
-        logo_url: `https://res.cloudinary.com/stephangriesel/image/upload/v1687318146/logo_cqhhz9.png`,
-        // Configure where our media assets are stored & served from
-        media_folder: "public/upload",
-        public_folder: "/upload",
-        // Configure the content collections
-        collections: [
-          {
-            name: "homeintro",
-            label: "Home Intro",
-            label_singular: "intro",
-            folder: "src/pages/content/homeintro",
-            create: true,
-            delete: true,
-            fields: [
-              { name: "title", widget: "string", label: "Post Title" },
-              {
-                name: "description",
-                widget: "string",
-                label: "Description",
-                required: true,
-              },
-              {
-                name: "tagline",
-                widget: "string",
-                label: "Tagline",
-                required: true,
-              },
-              {
-                name: "layout",
-                widget: "select",
-                default: "../../../layouts/General.astro",
-                options: [
-                  {
-                    label: "General",
-                    value: "../../../layouts/General.astro",
-                  },
-                ],
-              },
-            ],
-          },
-          {
-            name: "socials",
-            label: "Social Posts",
-            label_singular: "Social Post",
-            folder: "src/pages/content/social",
-            create: true,
-            delete: true,
-            fields: [
-              {
-                name: "title",
-                widget: "string",
-                label: "Title",
-                required: true,
-              },
-              {
-                name: "icon",
-                widget: "string",
-                label: "Icon",
-                required: true,
-              },
-              {
-                name: "url",
-                widget: "string",
-                label: "Url",
-                required: true,
-              },
-              {
-                name: "layout",
-                widget: "select",
-                default: "../../../layouts/General.astro",
-                options: [
-                  {
-                    label: "General Post",
-                    value: "../../../layouts/General.astro",
-                  },
-                ],
-              },
-            ],
-          },
-        ],
-      },
-    }),
-  ],
+      // Logo for backend
+      logo_url: `https://res.cloudinary.com/stephangriesel/image/upload/v1687318146/logo_cqhhz9.png`,
+      // Configure where our media assets are stored & served from
+      media_folder: "public/upload",
+      public_folder: "/upload",
+      // Configure the content collections
+      collections: [{
+        name: "homeintro",
+        label: "Home Intro",
+        label_singular: "intro",
+        folder: "src/pages/content/homeintro",
+        create: true,
+        delete: true,
+        fields: [{
+          name: "title",
+          widget: "string",
+          label: "Post Title"
+        }, {
+          name: "description",
+          widget: "string",
+          label: "Description",
+          required: true
+        }, {
+          name: "tagline",
+          widget: "string",
+          label: "Tagline",
+          required: true
+        }, {
+          name: "layout",
+          widget: "select",
+          default: "../../../layouts/General.astro",
+          options: [{
+            label: "General",
+            value: "../../../layouts/General.astro"
+          }]
+        }]
+      }, {
+        name: "socials",
+        label: "Social Posts",
+        label_singular: "Social Post",
+        folder: "src/pages/content/social",
+        create: true,
+        delete: true,
+        fields: [{
+          name: "title",
+          widget: "string",
+          label: "Title",
+          required: true
+        }, {
+          name: "icon",
+          widget: "string",
+          label: "Icon",
+          required: true
+        }, {
+          name: "url",
+          widget: "string",
+          label: "Url",
+          required: true
+        }, {
+          name: "layout",
+          widget: "select",
+          default: "../../../layouts/General.astro",
+          options: [{
+            label: "General Post",
+            value: "../../../layouts/General.astro"
+          }]
+        }]
+      }]
+    }
+  }), icon()]
 });
